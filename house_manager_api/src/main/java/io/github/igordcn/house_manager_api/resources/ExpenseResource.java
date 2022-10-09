4c4fe2d52947fca6837af462d24a6351e2b3db71
Igor de Carvalho Negócio <igorcnegocio@gmail.com>
2022-10-09 15:36:53 -0300

    feature: Implement endpoints for Expenses and Incomes

--- house_manager_api/src/main/java/io/github/igordcn/house_manager_api/resources/ExpenseResource.java
+++ house_manager_api/src/main/java/io/github/igordcn/house_manager_api/resources/ExpenseResource.java
@@ -1,8 +1,22 @@
 package io.github.igordcn.house_manager_api.resources;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDate;
+import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import io.github.igordcn.house_manager_api.entities.Expense;
 import io.github.igordcn.house_manager_api.services.ExpenseService;
 
 @RestController
@@ -13,6 +27,38 @@ public class ExpenseResource {
 
     public ExpenseResource(ExpenseService service) {
         this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Expense>> getPage(
+        @RequestParam UUID categoryId,
+        @RequestParam UUID destinationId,
+        @RequestParam UUID bankId,
+        @RequestParam LocalDate dateStart,
+        @RequestParam LocalDate dateEnd,
+        @RequestParam Pageable pageable
+    ) {
+        var expense = service.findByCategoryBankDestinationAndInterval(categoryId, destinationId, bankId, dateStart, dateEnd, pageable);
+        return ResponseEntity.ok(expense);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Expense> getById(@PathVariable UUID id) {
+        var expense = service.findById(id).orElseThrow();
+        return ResponseEntity.ok(expense);
+    }
+
+    @PostMapping
+    public ResponseEntity<Expense> add(@RequestBody Expense namedDto) {
+        var destination = service.save(namedDto);
+        var uri = UriComponentsBuilder.fromUriString("/expenses/" + destination.getId()).build().toUri();
+        return ResponseEntity.created(uri).body(destination);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remove(@PathVariable UUID id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- house_manager_api/src/main/java/io/github/igordcn/house_manager_api/resources/IncomeResource.java
+++ house_manager_api/src/main/java/io/github/igordcn/house_manager_api/resources/IncomeResource.java
@@ -1,16 +1,62 @@
 package io.github.igordcn.house_manager_api.resources;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDate;
+import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import io.github.igordcn.house_manager_api.entities.Income;
 import io.github.igordcn.house_manager_api.services.IncomeService;
 
 @RestController
 @RequestMapping("/incomes")
 public class IncomeResource {
+    
     private IncomeService service;
 
     public IncomeResource(IncomeService service) {
         this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Income>> getPage(
+        @RequestParam UUID originId,
+        @RequestParam String name,
+        @RequestParam LocalDate dateStart,
+        @RequestParam LocalDate dateEnd,
+        @RequestParam Pageable pageable
+    ) {
+        var income = service.findByNameAndOriginAndInterval(originId, name, dateStart, dateEnd, pageable);
+        return ResponseEntity.ok(income);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Income> getById(@PathVariable UUID id) {
+        var income = service.findById(id).orElseThrow();
+        return ResponseEntity.ok(income);
+    }
+
+    @PostMapping
+    public ResponseEntity<Income> add(@RequestBody Income namedDto) {
+        var destination = service.save(namedDto);
+        var uri = UriComponentsBuilder.fromUriString("/incomes/" + destination.getId()).build().toUri();
+        return ResponseEntity.created(uri).body(destination);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remove(@PathVariable UUID id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
