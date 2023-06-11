54eeebb251aa00b1db4a8bca4d1abc191297f30f
Igor de Carvalho Negócio <igorcnegocio@gmail.com>
2023-06-11 20:54:42 -0300

    Create the gateway project

---
+++ house_manager_gateway/src/main/java/io/github/igordcn/house_manager_gateway/HouseManagerGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.github.igordcn.house_manager_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseManagerGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseManagerGatewayApplication.class, args);
+	}
+
+}
---
+++ house_manager_gateway/src/test/java/io/github/igordcn/house_manager_gateway/HouseManagerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.igordcn.house_manager_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseManagerGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
